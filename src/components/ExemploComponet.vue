2fbb7c75706c6f4706dad863c9a2b7135154bec5
Fabio Rocha <fabiorochahelp@gmail.com>
2021-10-28 18:02:04 -0300

    Fist

---
+++ src/components/ExemploComponet.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Exemple</h1>
+</template>
+
+<script>
+
+export default defineComponent({
+  name: 'ExampleComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
